a54982cc833078765f18a8f671e770bb709150cb
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-03 16:20:23 +0300

    new things added

---
+++ control_flow/Main.java
@@ -0,0 +1,37 @@
+package control_flow;
+
+public class Main {
+    public static void main(String[] args){    
+        //comparison operators
+        int x =1;
+        int y =2;
+        System.out.println(x==y);
+            //<= != > < >=
+        //logical operators
+        int temp = 22;
+        boolean isWarm = temp > 20 && temp < 20;
+        System.out.println(isWarm);
+            // || !
+        //if statements
+        int temperature = 32;
+        if (temperature>30){
+            System.out.println("hot");
+        }else if (temperature > 20){
+            System.out.println("Good");
+        }else{
+            System.out.println("cold");
+        }
+        //if statements 2.0
+        int income = 120_000;
+        boolean hasHighIncome = (income >100_000);
+        System.out.println(hasHighIncome);
+        //Ternary operator
+        int money = 120_000;
+        String className = money > 100_000 ? "First" : "Economy";
+        System.out.println(className);
+        //Swicth Statements
+        
+
+
+    }
+}
