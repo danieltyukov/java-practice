28f11303fc2cbca3ce10ea8d1878273dacf3324c
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-05 00:34:24 +0300

    messing with objects

--- classes/Main.java
+++ classes/Main.java
@@ -2,16 +2,10 @@ package classes;
 
 public class Main {
 
-  // Classes consists of fields and objects
-
   public static void main(String[] args) {
-    // referencing the the classes and using its objects
-    var textBox1 = new TextBox();
-    textBox1.setText("Box 1");
-    System.out.println(textBox1.text.toUpperCase());
-
-    var textBox2 = new TextBox();
-    textBox2.setText("Box 2");
-    System.out.println(textBox2.text);
+    var employee = new Employee(50_000, 20);
+    Employee.printNumberOfEmployees();
+    int wage = employee.calculateWage();
+    System.out.println(wage);
   }
 }
--- classes/TextBox.java
+++ classes/TextBox.java
@@ -1,15 +1,14 @@
 package classes;
 
-//public is a access modifier if toher classes can have access to this one or not
 public class TextBox {
 
-  public String text = ""; // field
+  public String text = "";
 
   public void setText(String text) {
     this.text = text;
-  } // method
+  }
 
   public void clear() {
     text = "";
-  } // method2.0
+  }
 }
