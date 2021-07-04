934bec340173c7e56b0d5670880aead4c1502b44
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-04 23:01:02 +0300

    formatting2.0

--- clean_code/Main.java
+++ clean_code/Main.java
@@ -1,31 +1,30 @@
 package clean_code;
 
 public class Main {
-    // break down your code into methods
-    // here we create a method
 
-    // main method
-    public static void main(String[] args) {
-        greetUser("John", "Smith");
-        String returnedVal = farewellUser("Joe");
-        System.out.println(returnedVal);
-    }
+  // break down your code into methods
+  // here we create a method
 
-    // secondary method
-    // access modifiers can be public or private | public can be called outside
-    // class | vise-versa
-    // can be static or non-static | static belong to the class
-    // static method can only access static data and can call only static methods
-    // a method can return value or not | if not use void | else int,String etc...
-    // input to a method is defined inside () | if nothing leave empty
-    // it can then be used locally inside the method
-    public static void greetUser(String name, String lastName) {
-        System.out.println(name + lastName);
+  // main method
+  public static void main(String[] args) {
+    greetUser("John", "Smith");
+    String returnedVal = farewellUser("Joe");
+    System.out.println(returnedVal);
+  }
 
-    }
+  // secondary method
+  // access modifiers can be public or private | public can be called outside
+  // class | vise-versa
+  // can be static or non-static | static belong to the class
+  // static method can only access static data and can call only static methods
+  // a method can return value or not | if not use void | else int,String etc...
+  // input to a method is defined inside () | if nothing leave empty
+  // it can then be used locally inside the method
+  public static void greetUser(String name, String lastName) {
+    System.out.println(name + lastName);
+  }
 
-    public static String farewellUser(String name) {
-        return "Bye" + name;
-    }
-
+  public static String farewellUser(String name) {
+    return "Bye" + name;
+  }
 }
--- mortgagecalc/Main.java
+++ mortgagecalc/Main.java
@@ -4,81 +4,114 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-    final static byte MONTHS_IN_YEAR = 12;
-    final static byte PERCENT = 100;
+  
 
-    public static void main(String[] args) {
+  static final byte MONTHS_IN_YEAR = 12;
+  static final byte PERCENT = 100;
 
-        Scanner in = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
 
-        int principal = (int) readNumber("Principal: ", 1000, 1_000_000, in);
-        float annuelInterest = (float) readNumber("Annual Interest Rate: ", 1, 30, in);
-        byte years = (byte) readNumber("Period (Years): ", 1, 30, in);
-        in.close();
+    int principal = (int) readNumber("Principal: ", 1000, 1_000_000, in);
+    float annuelInterest = (float) readNumber(
+      "Annual Interest Rate: ",
+      1,
+      30,
+      in
+    );
+    byte years = (byte) readNumber("Period (Years): ", 1, 30, in);
+    in.close();
 
-        printMortgage(principal, annuelInterest, years);
-        printPaymentSchedule(principal, annuelInterest, years);
+    printMortgage(principal, annuelInterest, years);
+    printPaymentSchedule(principal, annuelInterest, years);
+  }
 
+  private static void printMortgage(
+    int principal,
+    float annuelInterest,
+    byte years
+  ) {
+    double mortgage = calculateMortgage(principal, annuelInterest, years);
+    String mortgageFormatted = NumberFormat
+      .getCurrencyInstance()
+      .format(mortgage);
+    System.out.println();
+    System.out.println("MORTGAGE");
+    System.out.println("--------");
+    System.out.println("Monthly Payments: " + mortgageFormatted);
+  }
+
+  private static void printPaymentSchedule(
+    int principal,
+    float annuelInterest,
+    byte years
+  ) {
+    System.out.println();
+    System.out.println("PAYMENT SCHEDULE");
+    System.out.println("----------------");
+    for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+      double balance = calculateBalance(
+        principal,
+        annuelInterest,
+        years,
+        month
+      );
+      System.out.println(NumberFormat.getCurrencyInstance().format(balance));
     }
+  }
 
-    private static void printMortgage(int principal, float annuelInterest, byte years) {
-        double mortgage = calculateMortgage(principal, annuelInterest, years);
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println();
-        System.out.println("MORTGAGE");
-        System.out.println("--------");
-        System.out.println("Monthly Payments: " + mortgageFormatted);
+  public static double readNumber(
+    String prompt,
+    double min,
+    double max,
+    Scanner in
+  ) {
+    double value;
+    while (true) {
+      System.out.print(prompt);
+      value = in.nextFloat();
+
+      if (value >= min && value <= max) {
+        break;
+      }
+
+      System.out.print("Enter value between " + min + " and " + max);
     }
+    return value;
+  }
 
-    private static void printPaymentSchedule(int principal, float annuelInterest, byte years) {
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
-            double balance = calculateBalance(principal, annuelInterest, years, month);
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-        }
-    }
+  public static double calculateBalance(
+    int principal,
+    float annuelInterest,
+    byte years,
+    short numberOfPaymentsMade
+  ) {
+    float monthlyInterest = annuelInterest / PERCENT / MONTHS_IN_YEAR;
+    float numberOfPayments = years * MONTHS_IN_YEAR;
 
-    public static double readNumber(String prompt, double min, double max, Scanner in) {
+    double balance =
+      principal *
+      (
+        Math.pow(1 + monthlyInterest, numberOfPayments) -
+        Math.pow(1 + monthlyInterest, numberOfPaymentsMade)
+      ) /
+      (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    return balance;
+  }
 
-        double value;
-        while (true) {
+  public static double calculateMortgage(
+    int principal,
+    float annuelInterest,
+    byte years
+  ) {
+    float monthlyInterest = annuelInterest / PERCENT / MONTHS_IN_YEAR;
+    float numberOfPayments = years * MONTHS_IN_YEAR;
 
-            System.out.print(prompt);
-            value = in.nextFloat();
+    double mortgage =
+      principal *
+      (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) /
+      (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
-            if (value >= min && value <= max) {
-                break;
-            }
-
-            System.out.print("Enter value between " + min + " and " + max);
-        }
-        return value;
-    }
-
-    public static double calculateBalance(int principal, float annuelInterest, byte years, short numberOfPaymentsMade) {
-
-        float monthlyInterest = annuelInterest / PERCENT / MONTHS_IN_YEAR;
-        float numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double balance = principal
-                * (Math.pow(1 + monthlyInterest, numberOfPayments)
-                        - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-        return balance;
-
-    }
-
-    public static double calculateMortgage(int principal, float annuelInterest, byte years) {
-
-        float monthlyInterest = annuelInterest / PERCENT / MONTHS_IN_YEAR;
-        float numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-
-        return mortgage;
-
-    }
+    return mortgage;
+  }
 }
