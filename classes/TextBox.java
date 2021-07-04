617c9b84dc20a9771f23de16b56e42f1c8ef6530
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-04 23:00:52 +0300

    formatting

--- classes/TextBox.java
+++ classes/TextBox.java
@@ -2,14 +2,14 @@ package classes;
 
 //public is a access modifier if toher classes can have access to this one or not
 public class TextBox {
-    public String text = ""; // field
 
-    public void setText(String text) {
-        this.text = text;
+  public String text = ""; // field
 
-    } // method
+  public void setText(String text) {
+    this.text = text;
+  } // method
 
-    public void clear() {
-        text = "";
-    }// method2.0
+  public void clear() {
+    text = "";
+  } // method2.0
 }
