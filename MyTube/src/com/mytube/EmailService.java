386ac1f6a9acea7b98a93c851890640c2e5638b9
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-07 02:05:35 +0300

    added mytube first version

---
+++ MyTube/src/com/mytube/EmailService.java
@@ -0,0 +1,9 @@
+package MyTube.src.com.mytube;
+
+public class EmailService {
+
+  public void sendEmail(User user) {
+    System.out.println("Notifying " + user.getEmail() + "...");
+    System.out.println("Done!\n");
+  }
+}
---
+++ MyTube/src/com/mytube/VideoEncoder.java
@@ -0,0 +1,9 @@
+package MyTube.src.com.mytube;
+
+public class VideoEncoder {
+
+  public void encode(Video video) {
+    System.out.println("Encoding video...");
+    System.out.println("Done!\n");
+  }
+}
