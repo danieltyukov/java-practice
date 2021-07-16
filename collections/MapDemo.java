46070ee3e179b57626579cea2d8f7e3aa3d9abfe
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-16 18:40:04 +0300

    added comments

--- collections/MapDemo.java
+++ collections/MapDemo.java
@@ -14,13 +14,16 @@ public class MapDemo {
     map.put(c2.getEmail(), c2);
 
     var exists = map.containsKey("e1");
+    System.out.println(exists);
 
     var unknown = new Customer("Unknown", "");
     var customer = map.get("e1");
+    System.out.println(customer);
     customer = map.getOrDefault("e1", unknown);
 
     map.replace("e1", new Customer("a++", "e1"));
 
+    //here we get the key of the map values as we loop through it
     for (var key : map.keySet()) System.out.println(key);
 
     for (var value : map.values()) System.out.println(value);
