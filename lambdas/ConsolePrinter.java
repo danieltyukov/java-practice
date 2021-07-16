265d623ce8b6bad081406f4a39470e7879d3c423
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-16 21:47:27 +0300

    formatted the code

--- lambdas/ConsolePrinter.java
+++ lambdas/ConsolePrinter.java
@@ -1,6 +1,7 @@
 package lambdas;
 
 public class ConsolePrinter implements Printer {
+
   @Override
   public void print(String message) {
     System.out.println(message);
--- lambdas/LambdasDemo.java
+++ lambdas/LambdasDemo.java
@@ -3,6 +3,7 @@ package lambdas;
 import java.util.function.UnaryOperator;
 
 public class LambdasDemo {
+
   public static void show() {
     UnaryOperator<Integer> square = n -> n * n;
     UnaryOperator<Integer> increment = n -> n + 1;
