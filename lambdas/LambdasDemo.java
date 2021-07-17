b857cf16634b01d4919cab43b88bf0a8d3f69c0a
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-17 15:39:19 +0300

    working with lambda

--- lambdas/LambdasDemo.java
+++ lambdas/LambdasDemo.java
@@ -1,6 +1,8 @@
 package lambdas;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class LambdasDemo {
@@ -9,6 +11,7 @@ public class LambdasDemo {
     UnaryOperator<Integer> square = n -> n * n;
     UnaryOperator<Integer> increment = n -> n + 1;
 
+    //we are chaining consumers with .andThen()
     var result = increment.andThen(square).apply(1);
     System.out.println(result);
 
@@ -20,5 +23,26 @@ public class LambdasDemo {
     }
     //declartive programming
     list.forEach(item -> System.out.println(item));
+
+    //Supplier Interface
+    Supplier<Double> supplier = () -> Math.random();
+    var random = supplier.get();
+    System.out.println(random);
+
+    //Functional Interface
+    Function<String, Integer> map = str -> str.length();
+    var length = map.apply("hello");
+    System.out.println(length);
+
+    //Composing Functions
+    Function<String, String> replaceColon = str -> str.replace(":", "=");
+    Function<String, String> addBraces = str -> "(" + str + ")";
+
+    //Declarative Programming
+    var res = replaceColon.andThen(addBraces).apply("key:value");
+    System.out.println(res);
+    //Another way of doing it
+    var res2 = addBraces.compose(replaceColon).apply("key:value");
+    System.out.println(res2);
   }
 }
