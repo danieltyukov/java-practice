8dd4f97ffc31152d6a60e0934785601eba16e106
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-18 16:53:18 +0300

    added concurrency

--- streams/Streams2Demo.java
+++ streams/Streams2Demo.java
@@ -1,17 +1,17 @@
 package streams;
 
-import java.util.List;
+// import java.util.List;
 // import java.util.stream.Collector;
 import java.util.stream.IntStream;
 
 public class Streams2Demo {
 
   public static void show() {
-    var movies = List.of(
-      new Movie("The Matrix", 10, Genre.THRILLER),
-      new Movie("The Dark Knight", 20, Genre.ACTION),
-      new Movie("Joker", 30, Genre.COMEDY)
-    );
+    // var movies = List.of(
+    //   new Movie("The Matrix", 10, Genre.THRILLER),
+    //   new Movie("The Dark Knight", 20, Genre.ACTION),
+    //   new Movie("Joker", 30, Genre.COMEDY)
+    // );
     //Grouping Elements
     // var groupByGenre = movies
     //   .stream()
